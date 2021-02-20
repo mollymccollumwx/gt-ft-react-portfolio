91050349228f2a4b1c981854e47ce1ce26b76004
Molly <mollymccollumwx@gmail.com>
2021-02-20 09:52:54 -0500

    scaffolded out basic pages and added the hashrouter to make sure it was working. Added bootstrap CDN to the index.html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import React from "react";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "./containers/Home/Home";
+import About from "./containers/About/About";
+import Contact from "./containers/Contact/Contact";
+import Portfolio from "./containers/Portfolio/Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component= {Home}/>
+          <Route exact path="/about" component= {About}/>
+          <Route exact path="/contact" component= {Contact}/>
+          <Route exact path="/portfolio" component= {Portfolio}/>
+          
+        </Switch>
+      </Router>
     </div>
   );
 }
